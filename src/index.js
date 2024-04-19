fda37e3c645b77b72cb4ada6889b30b2b20728e6
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2024-04-19 22:53:48 +0530

    connected to mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("her0000000");
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({path: './env'})
+
+
+connectDB()
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+// function connectDB(){}
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("error: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`app is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log(`Error : ${error}`);
+        throw error
+    }
+} )()
+
+*/
