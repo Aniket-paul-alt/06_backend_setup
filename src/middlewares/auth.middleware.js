2c232aff2791030b26691b354dd9f949df4c55d9
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2024-04-29 12:18:45 +0530

    some bug fixed

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -32,19 +32,22 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import { User } from "../models/user.models.js";
 
-export const verifyJWT = asyncHandler(async(req, res, next) => {
+const verifyJWT = asyncHandler(async(req, res, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        // console.log(token);
 
-        if (!token) {
+        if(!token) {
             throw new ApiError(401, "Unauthorized request")
         }
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        // console.log(decodedToken);
 
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+        // console.log(user);
 
-        if (!user) {
+        if(!user) {
             // NEXT_VIDEO: discuss about frontend
             throw new ApiError(401, "Invalid Access Token")
         }
@@ -56,3 +59,5 @@ export const verifyJWT = asyncHandler(async(req, res, next) => {
     }
 
 })
+
+export { verifyJWT }
